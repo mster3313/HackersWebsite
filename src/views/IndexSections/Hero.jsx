036694d24069df30af9af86c6ebf629df58b96e8
Kalana <edata.incore@gmail.com>
2019-10-03 14:45:59 +0530

    initial files

---
+++ src/views/IndexSections/Hero.jsx
@@ -0,0 +1,165 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import Particles from 'react-particles-js';
+
+// reactstrap components
+import { Button, Container, Row, Col } from "reactstrap";
+
+class Hero extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="position-relative">
+          {/* Hero for FREE version */}
+          <section className="section section-lg section-hero section-shaped">
+            {/* Background circles */}
+            <div className="shape shape-style-1 shape-default ">
+            <Particles params={{
+	    "particles": {
+	        "number": {
+	            "value": 160,
+	            "density": {
+	                "enable": false
+	            }
+	        },
+	        "size": {
+	            "value": 3,
+	            "random": true,
+	            "anim": {
+	                "speed": 4,
+	                "size_min": 0.3
+	            }
+	        },
+	        "line_linked": {
+	            "enable": false
+	        },
+	        "move": {
+	            "random": true,
+	            "speed": 1,
+	            "direction": "top",
+	            "out_mode": "out"
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "bubble"
+	            },
+	            "onclick": {
+	                "enable": true,
+	                "mode": "repulse"
+	            }
+	        },
+	        "modes": {
+	            "bubble": {
+	                "distance": 250,
+	                "duration": 2,
+	                "size": 0,
+	                "opacity": 0
+	            },
+	            "repulse": {
+	                "distance": 400,
+	                "duration": 4
+	            }
+	        }
+	    }
+	}}/>
+              {/* <span className="span-150" />
+              
+              <span className="span-50" />
+              <span className="span-50" />
+              <span className="span-75" />
+              <span className="span-100" />
+              <span className="span-75" />
+              <span className="span-50" />
+              <span className="span-100" />
+              <span className="span-50" />
+              <span className="span-100" /> */}
+            </div>
+            <Container className=" d-flex align-items-center py-lg">
+              <div className="col px-0">
+                <Row className="align-items-center justify-content-center">
+                  <Col className="text-center" lg="6">
+                    <img
+                      alt="..."
+                      className="img-fluid"
+                      src={require("assets/img/brand/logo-white.png")}
+                      style={{ width: "350px" }}
+                    />
+                    <p className="lead text-white">
+                    Lions Clubs International (LCI) was founded in 1917 as an affiliation of Lions Clubs.
+                    </p>
+                    <div className="btn-wrapper mt-5">
+                      {/* <Button
+                        className="btn-white btn-icon mb-3 mb-sm-0"
+                        color="default"
+                        
+                        size="lg"
+                      >
+                        <span className="btn-inner--icon mr-1">
+                          <i className="ni ni-cloud-download-95" />
+                        </span>
+                        <span className="btn-inner--text">Download React</span>
+                      </Button>{" "}
+                      <Button
+                        className="btn-icon mb-3 mb-sm-0"
+                        color="github"
+                        href="https://github.com/creativetimofficial/argon-design-system-react"
+                        size="lg"
+                        target="_blank"
+                      > */}
+                        {/* <span className="btn-inner--icon mr-1">
+                          <i className="fa fa-github" />
+                        </span>
+                        <span className="btn-inner--text">
+                          <span className="text-warning mr-1">Star us</span>
+                          on Github
+                        </span>
+                      </Button> */}
+                    </div>
+                   
+                  </Col>
+                </Row>
+              </div>
+            </Container>
+            {/* SVG separator */}
+            <div className="separator separator-bottom separator-skew zindex-100">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                preserveAspectRatio="none"
+                version="1.1"
+                viewBox="0 0 2560 100"
+                x="0"
+                y="0"
+              >
+                <polygon
+                  className="fill-white"
+                  points="2560 0 2560 100 0 100"
+                />
+              </svg>
+            </div>
+          </section>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Hero;
